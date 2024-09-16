da9819cdf39977bd9d18958a6246b6f80716b732
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-09-16 11:09:00 +0200

    Wariant minimum

--- NewDiceRoll/MainPage.xaml.cs
+++ NewDiceRoll/MainPage.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+
 
         public MainPage()
         {
@@ -11,14 +11,12 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // stwórz obiekt klasy Random - grenerator liczb losowych
+            Random r = new Random();
+            // wylosuj liczbę z zakresu od 1 do 6
+            int diceRoll = r.Next(1, 7);
+            // wyświetl wynik w guziku
+            CounterBtn.Text = "Wynik rzutu: " + diceRoll.ToString();
         }
     }
 
