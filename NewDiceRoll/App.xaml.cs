7e2c8f8d9ae7b5e190a4ad71f0f62fdd243de73f
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2024-09-16 11:05:14 +0200

    Dodaj pliki projektów.

---
+++ NewDiceRoll/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NewDiceRoll
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NewDiceRoll/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NewDiceRoll
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
